4e402ccd22c89dc377674d528891bb5aa81e7ddf
HolyKarter <el.holy.karter@gmail.com>
2026-03-12 23:38:41 -0400

    working of an frustrated version of the tail and head algorithm

---
+++ p2.py
@@ -0,0 +1,38 @@
+class Linked_list():
+    pass
+def FinderTailvsHead(LinkedList):
+    head = LinkedList 
+    current = h1
+
+    while current is not None:
+        tail = current.next
+        current = tail
+    return (head, tail)
+
+def sumofLinkedList(l1, l2):
+        return (head, tail)
+
+length1 = len(l1)
+length2 = len(l2) 
+    
+if length1 != length2: 
+    lowest = l2
+    biggest = l1
+    if length1 < length2:
+        length1, length2 = length2, length1
+        biggest, lowest =l2, l1 
+
+     delta = biggest - lowest
+     tail = FinderTailVsHead(lowest)[1] 
+    s = 0
+     while s <= delta:
+        new = Linked_list()
+        new.val = 0
+       tail.
+
+
+start = 0
+while start < len( l1):
+
+     
+     
